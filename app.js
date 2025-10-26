a8dd98ad757bbfff55ea7bc272b1d5116a14f194
Musadique Muslim <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2025-10-26 02:08:58 -0700

    add table

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
-function myFunction(){
+function myFunction() {
     let num = document.getElementById('a');
+    let print = document.getElementById('print');
+    print.innerHTML = "";
+    x = Number(prompt("Enter any number", 5))
+    y = Number(prompt("Table length", 10))
+    num.innerHTML = x;
 
-let print = document.getElementById('print')
-x = Number(prompt("Enter any number",5))
-y = Number(prompt("Table length",10))
-num.innerHTML=x;
-for(i=1; i<=y; i++){
-    print.innerHTML += `${x} x ${i} = ${x * i}<br/>`;
+    for (i = 1; i <= y; i++) {
+        print.innerHTML += `${x} x ${i} = ${x * i}<br/>`;
 
-
-}
+    }
 }
